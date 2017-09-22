8bf14d6d000b132eee75c4e1d15a3267a4932192
Thaddaeus Caldwell <tpcaldwell@gmail.com>
2017-09-22 13:28:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 
 def fizzbuzz(int)
-  if int % 3  == 0
+  if int % 5 == 0 && int % 3 == 0
+    "FizzBuzz"
+  elsif condition
+     int % 3  == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 5 == 0 && int % 3 == 0
-    "FizzBuzz"
-  else "nil"
-  end
+    end
 end
 fizzbuzz(3)
 fizzbuzz(5)
